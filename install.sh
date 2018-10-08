620ff0a29121c54d72d863c496b34fc839407639
常乐 <Leptune@users.noreply.github.com>
2018-10-08 16:12:39 +0800

    Update install.sh
    为加快安装速度，改为不拷贝整个git项目，而是只拷贝最新的分支

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-git clone --recursive https://github.com/Leptune/vim-for-coding.git ~/vim-for-coding
+git clone --recursive --depth=1 https://github.com/Leptune/vim-for-coding.git ~/vim-for-coding
 cd ~
 mv .vim .vimbak &> /dev/null
 mv .vimrc .vimrcbak &> /dev/null
 mv vim-for-coding .vim
 ln -s .vim/vimrc .vimrc
+echo 'success! enjoy it!'
+exit 0
